31e47da9667dceafae8ec41d17030419a06e09ef
Sekine Yuzuki <258451@st.takushoku-u.ac.jp>
2026-05-21 12:30:14 +0900

    変更した

--- kazuate.js
+++ kazuate.js
@@ -8,7 +8,7 @@ console.log('答え（デバッグ用）: ' + kotae);
 let kaisu = 0;
 
 // そのほか，必要に応じて変数を宣言してもよい
-
+let check =0;
 // ボタンを押した後の処理をする関数 hantei() の定義
 function hantei() {
   // ここから: 予想回数を1増やして，span#kaisu 要素のテキストを更新
@@ -18,10 +18,20 @@ function hantei() {
   // ここまで: 予想回数を1増やして，span#kaisu 要素のテキストを更新
   
   // ここから: テキストボックスに指定された数値を yoso に代入する
-  let yoso =Number(document.querySelector('input[name ="kaitou"]').value);;
+  let yoso =Number(document.querySelector('input[name ="kaitou"]').value);
   // ここまで: テキストボックスに指定された数値を yoso に代入する
   
   // ここから: 正解判定する
+  if (kaisu >=4 || check >0){
+    result.textContent ='答えは '+kotae+' でした．すでにゲームは終わっています';
+  }else if (yoso == kotae){
+    result.textContent ='正解です。おめでとう!';
+    check +=1; 
+  }else if (yoso >kotae){
+    result.textContent ='まちがい。答えはもっと小さいですよ';
+  }else if (yoso <kotae){
+    result.textContent ='まちがい。答えはもっと大きいですよ';
+  }
   // 　　　　  正解/不正解のときのメッセージを表示する
 
   // ここまで: 正解判定する
